e3b19f21c6873e5ddc8d71bdae9c5ab6c90687ec
sealand0508 <sealand.chen@gmail.com>
2024-08-11 01:42:00 +0800

    add from vscode home by unknow who

--- src/io01.py
+++ src/io01.py
@@ -1,1 +1,19 @@
-print ('Intellij Office Add 01')
+# encoding: utf-8
+import requests
+from bs4 import BeautifulSoup
+
+def get_google_news():
+    url = "https://news.google.com/news/"
+    response = requests.get(url)
+    # soup = BeautifulSoup(response.content,
+    soup = BeautifulSoup(response.content,"html.parser")
+    news_list = soup.find_all("item")
+    for news in news_list:
+        title = news.title.text
+        link = news.link.text
+        print(f"標題: {title}")
+        print(f"鏈接: {link}")
+        print("")
+
+if __name__ == "__main__":
+    get_google_news()
---
+++ vh03.py
@@ -0,0 +1,1 @@
+print ('This is add from home vscode 01 by sealand0508')
